14db51b72bb1b18519799fd139694beeeab220d0
jason2014-liu <379626076@qq.com>
2016-09-20 11:53:03 +0800

    update

---
+++ interframe-common/src/main/java/com/interframe/common/ExampleBusinessObject.java
@@ -0,0 +1,25 @@
+/**
+* TODO
+* @Project: interframe-common
+* @Title: ExampleBusinessObject.java
+* @Package org.interframe.common
+* @author jason
+* @Date 2016年9月20日 上午10:07:48
+* @Copyright
+* @Version 
+*/
+package com.interframe.common;
+
+/**
+ * TODO
+ * 
+ * @ClassName: ExampleBusinessObject
+ * @author jason
+ */
+public class ExampleBusinessObject {
+
+	public void doInit() {
+		// do the actual work
+		System.out.println("doInit method is invoked.");
+	}
+}
---
+++ interframe-common/src/test/java/com/interframe/common/ExampleBusinessObjectTest.java
@@ -0,0 +1,46 @@
+/**
+* TODO
+* @Project: interframe-common
+* @Title: ExampleBusinessObjectTest.java
+* @Package org.interframe.common
+* @author jason
+* @Date 2016年9月20日 上午10:46:30
+* @Copyright
+* @Version 
+*/
+package com.interframe.common;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.interframe.common.ExampleBusinessObject;
+
+/**
+* TODO
+* @ClassName: ExampleBusinessObjectTest
+* @author jason
+*/
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:applicationContext-schedule.xml"})
+public class ExampleBusinessObjectTest {
+
+	@Autowired
+	ApplicationContext ctx;
+	
+	@Test
+	public void testEmployee(){
+		ExampleBusinessObject obj =(ExampleBusinessObject) ctx.getBean("exampleBusinessObject");
+		obj.doInit();
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
